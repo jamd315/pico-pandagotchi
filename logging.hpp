3be7ad634f8922b6c045fd09801bed2a54ed21e7
lizardswimmer <lizardswimmer@gmail.com>
2022-08-29 19:29:39 -0500

    logging header guard

--- logging.hpp
+++ logging.hpp
@@ -1,3 +1,6 @@
+#ifndef _LOGGING_H
+#define _LOGGING_H
+
 typedef enum log_level_t
 {
     LOG_CRITICAL = 0,
@@ -13,3 +16,5 @@ typedef enum log_level_t
 #define LOG_USE_COLOR
 
 void pretty_log(log_level_t log_level, const char *log_source, const char *fmt, ...);
+
+#endif // Header guard
